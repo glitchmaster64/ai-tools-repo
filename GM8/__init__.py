6251f1a57b9442f9ba40c8f1f008d7d41a062d09
glitchx64 <44145041+glitchmaster64@users.noreply.github.com>
2026-07-15 01:39:43 +0530

    added inital stuff GM8, an extensible pythonic framework for using (liberated local) llms in custom pentest / web / appsec workflows

---
+++ GM8/__init__.py
@@ -0,0 +1,6 @@
+from .agent import Agent
+from .chat import Chat
+from .config import Config
+from . import capabilities
+
+__version__ = "0.0.1"
---
+++ GM8/__main__.py
@@ -0,0 +1,5 @@
+from . import Agent, Chat, Config
+from .capabilities import Capabilities
+
+if __name__=="__main__":
+    print('supm8')
